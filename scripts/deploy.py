f32fcee622a1fae2f6e3fc4d12f37c60f60a0542
unparalleled-js <yingthi@live.com>
2022-10-05 12:21:06 -0500

    feat: jules avi

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,29 +1,19 @@
 import click
 from ape import project
 from ape.api.networks import LOCAL_NETWORK_NAME
-from ape.cli import NetworkBoundCommand, ape_cli_context, get_user_selected_account, network_option
+from ape.cli import NetworkBoundCommand, ape_cli_context, network_option
 
 
-# default connect to a provider
-def main():
-    account = get_user_selected_account()
-    account.deploy(project.NFT)
-
-
-# perk you can add args unlike main method
 @click.command(cls=NetworkBoundCommand)
-@click.option("--uri", help="base uri for nft", default="dummy value")
 @ape_cli_context()
 @network_option()
-# cli_ctx must go first
-def cli(cli_ctx, network, uri):
-    """
-    Deploy the nft
-    """
+def cli(cli_ctx, network):
+    """Deploy the NFT contract."""
+
     network = cli_ctx.provider.network.name
     if network == LOCAL_NETWORK_NAME or network.endwith("-fork"):
-        account = cli_ctx.account_manager.test_accounts[0]
+        owner = cli_ctx.account_manager.test_accounts[0]
     else:
-        account = get_user_selected_account()
+        owner = cli_ctx.account_manager.load("main")
 
-    account.deploy(project.NFT, uri)
+    owner.deploy(project.JulesAvatar)
